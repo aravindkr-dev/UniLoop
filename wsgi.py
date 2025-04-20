7c922cd04f06b926489840fd0333ae5ed664cef7
ARAVIND.K.R <154773786+aravindkr-dev@users.noreply.github.com>
2025-04-20 20:28:17 +0530

    beta1

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ from app import app, socketio
 import os
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT" , 4000)
+    port = int(os.environ.get("PORT" , 4000))
     socketio.run(app , host='0.0.0.0' , port = port)
