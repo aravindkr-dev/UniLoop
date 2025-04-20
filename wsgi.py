3d8cabf196ec0caa3b709ae94c1727d1094010ac
ARAVIND.K.R <154773786+aravindkr-dev@users.noreply.github.com>
2025-04-20 20:33:35 +0530

    beta2

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
+# wsgi.py
 from app import app, socketio
-import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT" , 4000))
-    socketio.run(app , host='0.0.0.0' , port = port)
+# This exposes the socketio app to gunicorn
+# No __main__, no run()
